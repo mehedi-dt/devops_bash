0ef9d8aff2fb385a502ba4490f3aa45136c62980
Mehedi Hasan <108521059+mehedi-dt@users.noreply.github.com>
2025-04-02 03:53:26 +0600

    docker install script

---
+++ install_dependencies/docker_install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Update the package index and install required dependencies
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Set up the Docker repository
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Start and enable Docker service
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Add your user to the 'docker' group to run Docker without sudo
+sudo usermod -aG docker $USER
+
+# Install Composer
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+# Output Docker version
+docker --version
+
+# Test Docker installation with a simple command
+docker run hello-world
