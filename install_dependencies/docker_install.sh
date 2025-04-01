7f2c3ed1d34342c4b0abda343f6f8511b28711b6
Mehedi Hasan <108521059+mehedi-dt@users.noreply.github.com>
2025-04-02 04:51:39 +0600

    install latest compose

--- install_dependencies/docker_install.sh
+++ install_dependencies/docker_install.sh
@@ -22,7 +22,7 @@ sudo systemctl enable docker
 sudo usermod -aG docker $USER
 
 # Install Composer
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
